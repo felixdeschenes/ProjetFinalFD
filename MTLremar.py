76cea919381fbbee2827cd1f9613b86b8c241392
felixdeschenes <felixdeschenes23@gmail.com>
2017-04-15 21:33:46 -0400

    Update MTLremar.py
    Script Python qui, à partir du répertoire d'arbres publics de Montréal où les coordonnées longitude-latitude ont été ajoutées ("arbresMTLconsolideEDM5240.csv"), écrit un fichier .csv ("arbresremarquablesMTL-EDM5240.csv") qui ne contient que les spécimens dits «remarquables».

--- MTLremar.py
+++ MTLremar.py
@@ -2,22 +2,33 @@
 
 import csv
 
+# PRÉPARATION BASIQUE DE LA LECTURE DU FICHIER CSV QUI RÉPERTORIE LES ARBRES PUBLICS DE MONTRÉAL ET
+# OÙ LES COORDONNÉES DE LATITUDE ET LONGITUDE ONT ÉTÉ AJOUTÉES
+
 fichier = "arbresMTLconsolideEDM5240.csv"
 nouveaucsv = "arbresremarquablesMTL-EDM5240.csv"
 f1 = open(fichier)
 lignes = csv.reader(f1)
 next(lignes)
 
-listerema = [ ]
+# À L'AIDE D'UNE BOUCLE, ON DÉFINIT UNE VARIABLE "rema" QUI CIBLE LA VALEUR DE LA COLONNE [18] DE CHAQUE LIGNE DU CSV.
+# CETTE COLONNE CONTIENT LA MENTION OU NON DE «REMARQUABILITÉ».
 
 for arbre in lignes:
     rema = str.strip(arbre[18])
+    
+    # SI "rema" ÉQUIVAUT À 'O' (OUI), ALORS NOTRE LIGNE IDENTIFIE UN ARBRE REMARQUABLE ET EST DÉFINIE PAR LA VARIABLE "mtl2"
     if "O" in rema:
         mtl2 = arbre
+        
+        # AFIN DE SUIVRE LA PROGRESSION DE L'IDENTIFICATION DES ARBRES REMARQUABLES, ON DEMANDE D'IMPRIMER LES VALEURS RETENUES.
         print(mtl2)
+        
+        # J'ÉCRIS LES INFORMATIONS DES ARBRES REMARQUABLES ("mtl2") DANS DANS UN NOUVEAU FICHIER CSV ("arbresremarquablesMTL-EDM5240.csv").
         écrire = open(nouveaucsv,"a")
         pouet = csv.writer(écrire)
         pouet.writerow(mtl2)
+    
+    # POUR TOUS LES ARBRES QUI NE SONT PAS REMARQUABLES, ON DEMANDE À PYTHON DE PASSER SON TOUR ET DE RESTER INACTIF.
     else:
         pass
- 
